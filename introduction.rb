776e3fc6d89e37414da1f52d05d15ccac98214a8
Lucky Hudda <mr.luckyhudda@gmail.com>
2024-02-17 19:04:30 +0530

    introduction-file

---
+++ introduction.rb
@@ -0,0 +1,9 @@
+first_name = "Lucky"
+last_name = "Hudda"
+full_name = first_name + " " + last_name
+email_address = 'Lucky@example.com'
+phone_number = 1234567890
+address =  '12345 Jaipur, raj.'
+
+puts first_name, last_name, email_address, phone_number, address
+
